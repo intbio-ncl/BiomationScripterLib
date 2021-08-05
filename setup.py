bf66a179f5ad193fc6f638aa5887edd3cbdd7f2a
Brad0440 <34337908+Brad0440@users.noreply.github.com>
2021-08-05 11:06:15 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BiomationScripter',
+      version='0.0',
+      description='Tools for scripting bio-automation protocols',
+      author='Bradley Brown',
+      author_email='bradley.brown4@hotmail.co.uk',
+      packages=['BiomationScripter', 'BiomationScripter.EchoProto', 'BiomationScripter.OTProto'],
+     )
