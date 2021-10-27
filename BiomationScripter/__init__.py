a3c27b6590ea1cb6a35798c5ef9f66ac5009b9b4
Brad0440 <34337908+Brad0440@users.noreply.github.com>
2021-10-27 10:31:58 +0100

    Formatting updates and bug fixes to generic BMS tools

--- BiomationScripter/__init__.py
+++ BiomationScripter/__init__.py
@@ -9,22 +9,20 @@ import pandas as pd
 class NegativeVolumeError(Exception):
     pass
 
-class UnknownLabware(Exception):
-    pass
-
-class UnknownHardware(Exception):
-    pass
-
 class RobotConfigurationError(Exception):
     pass
 
 class BMSTemplateError(Exception):
     pass
 
+class LabwareError(Exception):
+    pass
+
 class OutOFSourceMaterial(Exception):
     pass
 
 #####################
+# Classes
 
 class PlateLayout:
     def __init__(self, Name, Type):
@@ -35,57 +33,9 @@ class PlateLayout:
         self.content = {}
         self.well_range = None
 
-    def check_well(self, Well):
-        Row = Well[0]
-        Column = int(Well[1:])
-        InRange = None
-        if Row > chr(64 + self.rows):
-            InRange = False
-        elif Column > self.columns:
-            InRange = False
-        else:
-            InRange = True
-        return(InRange)
-
-    def clone_format(self, New_Name):
-        # Does not copy content
-        name = New_Name
-        type = self.type
-        rows = self.rows
-        columns = self.columns
-        plate_copy = PlateLayout(name, type)
-        plate_copy.define_format(rows, columns)
-        return(plate_copy)
-
     def define_format(self, Rows, Columns):
         self.rows = Rows
         self.columns = Columns
-
-    def update_volume_in_well(self, Volume, Reagent, Well):
-        if not Well in self.get_content().keys():
-            raise ValueError("{} has not been previously defined. Add content to this well using the `add_content` method.".format(Well))
-        well_content = self.get_content()[Well]
-        for content in well_content:
-            if content[0] == Reagent:
-                content[1] = float(Volume)
-
-    def add_content(self, Well, Reagent, Volume, Liquid_Class = False):
-        # TODO: * Check if Well exists in the plate
-        #       * Allow well ranges to span multiple columns
-        #       * Don't overwrite current content if a well range is specified
-        if Volume < 0:
-            raise NegativeVolumeError
-
-        # Volume should always be uL
-        if Liquid_Class == False:
-            Liquid_Class = "Unknown"
-        if ":" in Well:
-            for w in well_range(Well):
-                self.add_content(w, Reagent, Volume, Liquid_Class)
-        elif Well in self.content:
-            self.content[Well].append([Reagent,float(Volume), Liquid_Class])
-        else:
-            self.content[Well] = [ [Reagent,float(Volume), Liquid_Class] ]
 
     def get_well_range(self, Well_Range=None, Use_Outer_Wells = True):
         n_rows = self.rows
@@ -111,17 +61,51 @@ class PlateLayout:
 
         return(wells)
 
+    def check_well(self, Well):
+        Row = Well[0]
+        Column = int(Well[1:])
+        InRange = None
+        if Row > chr(64 + self.rows):
+            InRange = False
+        elif Column > self.columns:
+            InRange = False
+        else:
+            InRange = True
+        return(InRange)
+
+    def clone_format(self, New_Name):
+        # Does not copy content
+        name = New_Name
+        type = self.type
+        rows = self.rows
+        columns = self.columns
+        plate_copy = PlateLayout(name, type)
+        plate_copy.define_format(rows, columns)
+        return(plate_copy)
+
+    def add_content(self, Well, Reagent, Volume, Liquid_Class = False):
+        # TODO: * Check if Well exists in the plate
+        #       * Allow well ranges to span multiple columns
+        #       * Don't overwrite current content if a well range is specified
+        if Volume < 0:
+            raise NegativeVolumeError
+
+        # Volume should always be uL
+        if Liquid_Class == False:
+            Liquid_Class = "Unknown"
+        if ":" in Well:
+            for w in well_range(Well):
+                self.add_content(w, Reagent, Volume, Liquid_Class)
+        elif Well in self.content:
+            self.content[Well].append([Reagent,float(Volume), Liquid_Class])
+        else:
+            self.content[Well] = [ [Reagent,float(Volume), Liquid_Class] ]
+
     def get_content(self):
         return(self.content)
 
-    def clear_content(self):
-        self.content = {}
-
-    def clear_content_from_well(self, Well):
-        del self.content[Well]
-
     def get_occupied_wells(self):
-        return(self.get_content().keys())
+        return(list(self.get_content().keys()))
 
     def get_wells_containing_liquid(self, Liquid_Name):
         wells_to_return = []
@@ -131,8 +115,21 @@ class PlateLayout:
             for liquid in content[well]:
                 if liquid[0] == Liquid_Name:
                     wells_to_return.append(well)
-
         return(wells_to_return)
+
+    def clear_content(self):
+        self.content = {}
+
+    def clear_content_from_well(self, Well):
+        del self.content[Well]
+
+    def update_volume_in_well(self, Volume, Reagent, Well):
+        if not Well in self.get_content().keys():
+            raise LabwareError("{} has not been previously defined. Add content to this well using the `add_content` method.".format(Well))
+        well_content = self.get_content()[Well]
+        for content in well_content:
+            if content[0] == Reagent:
+                content[1] = float(Volume)
 
     def print(self):
         print("Information for " + self.name)
@@ -145,6 +142,7 @@ class PlateLayout:
                 content_return += (well+"\t"+str(c[1])+"\t\t"+c[2]+"\t\t"+c[0]+ "\n")
                 print(well+"\t"+str(c[1])+"\t\t"+c[2]+"\t\t"+c[0])
         return(content_return)
+
 
     # create a dummy PlateLayout object before running this method
     def import_plate(self, filename, path="~", ext=".xlsx"):
@@ -252,24 +250,31 @@ class Liquids:
 #
 #     return(wells)
 
-def Create_Plates_Needed(Plate_Format, N_Wells_Needed, N_Wells_Available):
+##########################################
+# Functions
+
+def Create_Plates_Needed(Plate_Format, N_Wells_Needed, N_Wells_Available = "All"):
+    if not type(N_Wells_Available) is int:
+        if N_Wells_Available == "All":
+            N_Wells_Available = Plate_Format.rows * Plate_Format.cols
+        else:
+            raise ValueError("`N_Wells_Available` should either be an integer, or 'All'.")
+
     N_Plates_Needed = math.ceil(N_Wells_Needed/N_Wells_Available)
-    Plates = []
-    for plate_n in range(0, N_Plates_Needed):
+    Plates = [Plate_Format]
+    for plate_n in range(1, N_Plates_Needed):
         Plate_Name = Plate_Format.name + str(plate_n)
         Plates.append(Plate_Format.clone_format(Plate_Name))
     return(Plates)
 
-def Lrange(L1,L2): # Between L1 and L2 INCLUSIVE of L1 and L2
-    L1 = ord(L1.upper())
-    L2 = ord(L2.upper())
-    for L in range(L1,L2+1):
-        yield(chr(L))
 
-def Labware_Row_To_Index(row):
-    return(ord(row.upper()) - ord("A"))
 
-def well_range(Wells, Labware_Format = None, Direction = "Horizontal"):
+def well_range(Wells, Labware_Format = None, Direction = "Horizontal", Box = True):
+    if not Direction == "Horizontal" and not Direction = "Vertical":
+        raise ValueError("`Direction` must be either 'Horizontal' or 'Vertical'")
+
+    if not Labware_Format and not Box:
+        raise LabwareError("`Box` can only be `False` when `Labware_Format` is specified")
 
     if not Labware_Format:
         Well = Wells
@@ -278,13 +283,17 @@ def well_range(Wells, Labware_Format = None, Direction = "Horizontal"):
         lastL = last[0]
         firstN = int(first[1:len(first)])
         lastN = int(last[1:len(last)])
-        rows = []
-        cols = []
         wells = []
-        for L in Lrange(firstL,lastL):
+        if Direction == "Horizontal":
+            for L in _Lrange(firstL,lastL):
+                for N in range(firstN,lastN+1):
+                    wells.append(L+str(N))
+            return(wells)
+        elif Direction == "Vertical":
             for N in range(firstN,lastN+1):
-                wells.append(L+str(N))
-        return(wells)
+                for L in _Lrange(firstL,lastL):
+                    wells.append(L+str(N))
+            return(wells)
 
     else:
         # Labware_Format = [n_rows,n_columns]
@@ -298,14 +307,14 @@ def well_range(Wells, Labware_Format = None, Direction = "Horizontal"):
         last_col = int(last_well[1:])
 
         # Check if first and last wells are in range
-        if (Labware_Row_To_Index(first_row) > end_row - 1) or (Labware_Row_To_Index(last_row) > end_row - 1) or (first_col > end_col) or (last_col > end_col):
+        if (_Labware_Row_To_Index(first_row) > end_row - 1) or (_Labware_Row_To_Index(last_row) > end_row - 1) or (first_col > end_col) or (last_col > end_col):
             raise ValueError("Wells are not in range of specified format")
 
         wells = []
 
         if Direction == "Horizontal":
             rows = []
-            for r in Lrange(first_row, last_row):
+            for r in _Lrange(first_row, last_row):
                 rows.append(r)
 
             first_col_of_row = first_col
@@ -321,10 +330,10 @@ def well_range(Wells, Labware_Format = None, Direction = "Horizontal"):
 
         elif Direction == "Vertical":
             rows = []
-            for r in Lrange("A", chr(64 + end_row)):
+            for r in _Lrange("A", chr(64 + end_row)):
                 rows.append(r)
 
-            first_row_of_col_index = Labware_Row_To_Index(first_row)
+            first_row_of_col_index = _Labware_Row_To_Index(first_row)
 
             for col in range(first_col, last_col + 1):
                 for row in rows[first_row_of_col_index:]:
@@ -335,5 +344,19 @@ def well_range(Wells, Labware_Format = None, Direction = "Horizontal"):
                     if well == last_well:
                         return(wells)
 
+## Private ##
+def _Lrange(L1,L2): # Between L1 and L2 INCLUSIVE of L1 and L2
+    L1 = ord(L1.upper())
+    L2 = ord(L2.upper())
+    for L in range(L1,L2+1):
+        yield(chr(L))
+
+def _Labware_Row_To_Index(row):
+    return(ord(row.upper()) - ord("A"))
+
+
+## To be deprecated ##
+
 def aliquot_calculator(Volume_Required, Volume_Per_Aliquot, Dead_Volume = 0):
+    print("This will soon be deprecated")
     return(math.ceil(Volume_Required/(Volume_Per_Aliquot + Dead_Volume)))
