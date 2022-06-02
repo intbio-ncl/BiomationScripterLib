b94f1dcd8936ef08f7578e1a05abc049931c829c
Brad0440 <b.bradley2@newcastle.ac.uk>
2022-06-02 11:47:10 +0100

    EP PCR Template: Option for multiple DNA volumes

--- BiomationScripter/EchoProto/Templates.py
+++ BiomationScripter/EchoProto/Templates.py
@@ -155,6 +155,7 @@ class PCR(_EchoProto.EchoProto_Template):
         Master_Mix: str = None,
         Master_Mix_Stock_Conc = None,
         Repeats: int = 1,
+        DNA_Amounts = None,
         **kwargs
     ):
 
@@ -196,7 +197,11 @@ class PCR(_EchoProto.EchoProto_Template):
         # Default DNA and primer amounts #
         ##################################
         # Default DNA amounts in uL for 5 uL reactions, and 1 - 1000 ng/uL stock concentration
-        self.dna_amount = 1
+        if not DNA_Amounts:
+            self.dna_amounts = [1 * self.volume/self.__default_volume]
+        else:
+            self.dna_amounts = DNA_Amounts
+
         # Default primer amounts in uL for 5 uL reactions, and 10 μM stock concentration
         self.primer_amount = 0.25
 
@@ -212,7 +217,7 @@ class PCR(_EchoProto.EchoProto_Template):
         # Calculate number of destination plates required #
         ###################################################
         # Determine final number of reactions, and hence number of destination wells required
-        n_reactions = len(self.reactions) * self.repeats
+        n_reactions = len(self.reactions) * self.repeats * len(self.dna_amounts)
 
         # Create the number of destination plates needed as layout objects
         extra_destination_layouts = _BMS.Create_Plates_Needed(
@@ -233,20 +238,9 @@ class PCR(_EchoProto.EchoProto_Template):
         volume_factor = self.volume/self.__default_volume
 
         # Use the volume factor to calculate the reagent volumes
-        dna_amount = self.dna_amount * volume_factor
         primer_amount = self.primer_amount * volume_factor
 
-        # Determine if a mastermix is being used
-        if self.master_mix:
-            master_mix_amount = self._master_mix_amount * volume_factor
-            reagent_amount = master_mix_amount + dna_amount + (2 * primer_amount)
-        else:
-            dNTPs_amount = self._dNTPs_amount * volume_factor
-            buffer_amount = self._buffer_amount * volume_factor
-            polymerase_amount = self._polymerase_amount * volume_factor
-            reagent_amount = dNTPs_amount + buffer_amount + polymerase_amount + dna_amount + (2 * primer_amount)
 
-        water_amount = self.volume - reagent_amount
 
         ########################################
         # Add liquids to destination layout(s) #
@@ -258,38 +252,58 @@ class PCR(_EchoProto.EchoProto_Template):
 
         # For every reaction
         for reaction in self.reactions:
-            for rep in range(0, self.repeats):
-                # Get the current destination plate and well
-                current_destination_plate = self.destination_plate_layouts[destination_plate_index]
-                current_destination_well = current_destination_plate.available_wells[destination_well_index]
+            for dna_amount in self.dna_amounts:
+                # Determine if a mastermix is being used
+                if self.master_mix:
+                    master_mix_amount = self._master_mix_amount * volume_factor
+                    reagent_amount = master_mix_amount + dna_amount + (2 * primer_amount)
+                else:
+                    dNTPs_amount = self._dNTPs_amount * volume_factor
+                    buffer_amount = self._buffer_amount * volume_factor
+                    polymerase_amount = self._polymerase_amount * volume_factor
+                    reagent_amount = dNTPs_amount + buffer_amount + polymerase_amount + dna_amount + (2 * primer_amount)
 
-                # Add the reagents, water, and DNA to the destinaton plate
-                try:
-                    current_destination_plate.add_content(current_destination_well, self.water, water_amount)
-                    current_destination_plate.add_content(current_destination_well, reaction[0], dna_amount)
-                    current_destination_plate.add_content(current_destination_well, reaction[1], primer_amount)
-                    current_destination_plate.add_content(current_destination_well, reaction[2], primer_amount)
+                water_amount = self.volume - reagent_amount
 
-                    if self.master_mix:
-                        current_destination_plate.add_content(current_destination_well, self.master_mix, master_mix_amount)
-                    else:
-                        current_destination_plate.add_content(current_destination_well, self.dNTPs, dNTPs_amount)
-                        current_destination_plate.add_content(current_destination_well, self.buffer, buffer_amount)
-                        current_destination_plate.add_content(current_destination_well, self.polymerase, polymerase_amount)
+                for rep in range(0, self.repeats):
+                    # Get the current destination plate and well
+                    current_destination_plate = self.destination_plate_layouts[destination_plate_index]
+                    current_destination_well = current_destination_plate.available_wells[destination_well_index]
 
-                    # Label the well
-                    current_destination_plate.add_well_label(current_destination_well, "{}-{}-{}-{}".format(reaction[0], reaction[1], reaction[2], rep))
-                # Raise a more relevant error message if NegativeVolumeError occurs
-                except _BMS.NegativeVolumeError:
-                    raise _BMS.NegativeVolumeError("This assembly is above the reaction volume: {}, {}".format(assembly, ratio))
+                    # Add the reagents, water, and DNA to the destinaton plate
+                    try:
+                        current_destination_plate.add_content(current_destination_well, self.water, water_amount)
+                        current_destination_plate.add_content(current_destination_well, reaction[0], dna_amount)
+                        current_destination_plate.add_content(current_destination_well, reaction[1], primer_amount)
+                        current_destination_plate.add_content(current_destination_well, reaction[2], primer_amount)
 
-                # Iterate to the next destination well
-                destination_well_index += 1
-                # Check if the current destinaton plate is full
-                if destination_well_index - 1 == len(current_destination_plate.available_wells):
-                    # If so, iterate to the first well of the next destination plate
-                    destination_plate_index += 1
-                    destination_well_index = 0
+                        if self.master_mix:
+                            current_destination_plate.add_content(current_destination_well, self.master_mix, master_mix_amount)
+                        else:
+                            current_destination_plate.add_content(current_destination_well, self.dNTPs, dNTPs_amount)
+                            current_destination_plate.add_content(current_destination_well, self.buffer, buffer_amount)
+                            current_destination_plate.add_content(current_destination_well, self.polymerase, polymerase_amount)
+
+                        # Label the well
+                        current_destination_plate.add_well_label(current_destination_well, "{}-{}-{}-DNA_Vol({})-{}".format(reaction[0], reaction[1], reaction[2], dna_amount, rep))
+                    # Raise a more relevant error message if NegativeVolumeError occurs
+                    except _BMS.NegativeVolumeError:
+                        raise _BMS.NegativeVolumeError("This assembly is above the reaction volume: {}, {}".format(assembly, ratio))
+
+                    # Iterate to the next destination well
+                    destination_well_index += 1
+                    # Check if the current destinaton plate is full
+                    if destination_well_index - 1 == len(current_destination_plate.available_wells):
+                        # If so, iterate to the first well of the next destination plate
+                        destination_plate_index += 1
+                        destination_well_index = 0
+
+        # Sanity check
+        for dest in self.destination_plate_layouts:
+            for well in dest.get_occupied_wells():
+                reagents = dest.get_liquids_in_well(well)
+                if not sum([dest.get_volume_of_liquid_in_well(reagent, well) for reagent in reagents]) == self.volume:
+                    raise _BMS.BMSTemplateError("Internal Calculation Error. Please raise as an issue on the GitHub.")
 
         # Generate the picklists
         self.create_picklists()
