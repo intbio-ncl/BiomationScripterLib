582fe59b76adc93f50bb5217fd733e30b7e5bcfe
Brad0440 <b.bradley2@newcastle.ac.uk>
2022-08-17 09:33:00 +0100

    More `heat_shock_template` tests

--- tests/test_biomation.py
+++ tests/test_biomation.py
@@ -494,9 +494,9 @@ class TestOTProto:
             assert protocol.deck[pos] is None
 
 
-class TestOTProtoTemplates:
-    def test_heat_shock_transformation(self):
+class Test_Heat_Shock_Transformation:
 
+    def test_with_temp_module(self):
         protocol = OT2.get_protocol_api('2.11')
         protocol.home()
 
@@ -619,4 +619,396 @@ class TestOTProtoTemplates:
         Transformation.custom_labware_dir = Custom_Labware_Dir
         Transformation.run()
 
-        assert len(protocol.commands()) == 897
+        assert len(protocol.commands()) == 898
+
+    def test_with_cooled_cells(self):
+        protocol = OT2.get_protocol_api('2.11')
+        protocol.home()
+
+
+        metadata = {
+            'protocolName': 'Transformation Example',
+            'author': 'Bradley Brown',
+            'author-email': 'b.bradley2@newcastle.ac.uk',
+            'user': '',
+            'user-email': '',
+            'source': 'Generated using BiomationScripter',
+            'apiLevel': '2.11',
+            'robotName': 'RobOT2' # This is the name of the OT2 you plan to run the protocol on
+        }
+
+        Custom_Labware_Dir = "Resources/For Docs/custom_labware"
+
+        DNA_Plate_Wells = [
+            "B2",
+            "B3",
+            "B4",
+            "B5",
+            "B6",
+            "B7",
+            "B8",
+            "B9",
+            "B10",
+            "B11",
+            "B12",
+        ]
+
+        DNA_Plate_Content = [
+            "Assembly 1",
+            "Assembly 2",
+            "Assembly 3",
+            "Assembly 4",
+            "Assembly 5",
+            "Assembly 6",
+            "Assembly 7",
+            "Assembly 8",
+            "Assembly 9",
+            "Assembly 10",
+            "Assembly 11",
+        ]
+
+        DNA_Plate = bms.Labware_Layout("DNA Plate", "appliedbiosystemsmicroampoptical_384_wellplate_20ul")
+
+        DNA_Plate.bulk_add_content(
+            Wells = DNA_Plate_Wells,
+            Reagents = DNA_Plate_Content,
+            Volumes = 20
+        )
+
+        # DNA Tubes Info #
+
+        DNA_Tubes_Wells = [
+            "A1",
+            "A2"
+        ]
+
+        DNA_Tubes_Content = [
+            "Positive",
+            "Negative"
+        ]
+
+        DNA_Tubes = bms.Labware_Layout("DNA Tubes", "3dprinted_24_tuberack_1500ul")
+
+        DNA_Tubes.bulk_add_content(
+            Wells = DNA_Tubes_Wells,
+            Reagents = DNA_Tubes_Content,
+            Volumes = 20
+        )
+
+        DNA_Source_Layouts = [DNA_Plate, DNA_Tubes]
+
+        # Other labware and material info #
+
+        Competent_Cells_Source_Type = "3dprinted_24_tuberack_1500ul"
+
+        Comp_Cells_Modules = ["temperature module gen2"]
+
+        Media_Source_Type = "3dprinted_15_tuberack_15000ul"
+        Transformation_Destination_Type = "nunclondeltasurface163320_96_wellplate_250ul"
+
+        Competent_Cells_Aliquot_Vol = 40 # uL
+        Media_Aliquot_Vol = 5000 # uL
+
+        # Protocol Parameters #
+
+        DNA_Per_Transformation = 3 # uL
+        Competent_Cells_Volume = 20 # uL
+        Final_Volume = 100 # uL
+        Heat_Shock_Time = 30 # Seconds
+        Heat_Shock_Temp = 42 # celcius
+        Wait_Before_Shock = 300 # seconds
+        Reps = 1
+
+        # Starting Tips #
+
+        Starting_20uL_Tip = "A8"
+        Starting_300uL_Tip = "A1"
+
+        Transformation = otproto_templates.Heat_Shock_Transformation(
+            Protocol=protocol,
+            Name=metadata["protocolName"],
+            Metadata=metadata,
+            DNA_Source_Layouts=DNA_Source_Layouts,
+            Competent_Cells_Source_Type=Competent_Cells_Source_Type,
+            Transformation_Destination_Type=Transformation_Destination_Type,
+            Media_Source_Type=Media_Source_Type,
+            DNA_Volume_Per_Transformation=DNA_Per_Transformation,
+            Competent_Cell_Volume_Per_Transformation=Competent_Cells_Volume,
+            Transformation_Final_Volume=Final_Volume,
+            Heat_Shock_Time=Heat_Shock_Time,
+            Heat_Shock_Temp=Heat_Shock_Temp,
+            Media_Aliquot_Volume=Media_Aliquot_Vol,
+            Competent_Cells_Aliquot_Volume=Competent_Cells_Aliquot_Vol,
+            Wait_Before_Shock=Wait_Before_Shock,
+            Replicates=Reps,
+            Cooled_Cells_Modules=Comp_Cells_Modules,
+            Starting_20uL_Tip = Starting_20uL_Tip,
+            Starting_300uL_Tip = Starting_300uL_Tip,
+        )
+        Transformation.custom_labware_dir = Custom_Labware_Dir
+        Transformation.run()
+
+        assert len(protocol.commands()) == 899
+
+        assert protocol.commands().count("Setting Temperature Module temperature to 4.0 °C (rounded off to nearest integer)") == 3
+
+        assert "Pausing robot operation: This protocol uses 7 aliquots of 40 uL competent cells, located at [A1 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3, B1 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3, C1 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3, D1 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3, A2 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3, B2 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3, C2 of 3dprinted 24 Tube Rack 1500 ÂµL on Temperature Module GEN2 on 3]" in protocol.commands()
+
+    def test_with_thermocycler(self):
+
+        protocol = OT2.get_protocol_api('2.11')
+        protocol.home()
+
+
+        metadata = {
+            'protocolName': 'Transformation Example',
+            'author': 'Bradley Brown',
+            'author-email': 'b.bradley2@newcastle.ac.uk',
+            'user': '',
+            'user-email': '',
+            'source': 'Generated using BiomationScripter',
+            'apiLevel': '2.11',
+            'robotName': 'RobOT2' # This is the name of the OT2 you plan to run the protocol on
+        }
+
+        Custom_Labware_Dir = "Resources/For Docs/custom_labware"
+
+        DNA_Plate_Wells = [
+            "B2",
+            "B3",
+            "B4",
+            "B5",
+            "B6",
+            "B7",
+            "B8",
+            "B9",
+            "B10",
+            "B11",
+            "B12",
+        ]
+
+        DNA_Plate_Content = [
+            "Assembly 1",
+            "Assembly 2",
+            "Assembly 3",
+            "Assembly 4",
+            "Assembly 5",
+            "Assembly 6",
+            "Assembly 7",
+            "Assembly 8",
+            "Assembly 9",
+            "Assembly 10",
+            "Assembly 11",
+        ]
+
+        DNA_Plate = bms.Labware_Layout("DNA Plate", "appliedbiosystemsmicroampoptical_384_wellplate_20ul")
+
+        DNA_Plate.bulk_add_content(
+            Wells = DNA_Plate_Wells,
+            Reagents = DNA_Plate_Content,
+            Volumes = 20
+        )
+
+        # DNA Tubes Info #
+
+        DNA_Tubes_Wells = [
+            "A1",
+            "A2"
+        ]
+
+        DNA_Tubes_Content = [
+            "Positive",
+            "Negative"
+        ]
+
+        DNA_Tubes = bms.Labware_Layout("DNA Tubes", "3dprinted_24_tuberack_1500ul")
+
+        DNA_Tubes.bulk_add_content(
+            Wells = DNA_Tubes_Wells,
+            Reagents = DNA_Tubes_Content,
+            Volumes = 20
+        )
+
+        DNA_Source_Layouts = [DNA_Plate, DNA_Tubes]
+
+        # Other labware and material info #
+
+        Competent_Cells_Source_Type = "3dprinted_24_tuberack_1500ul"
+        Media_Source_Type = "3dprinted_15_tuberack_15000ul"
+        Transformation_Destination_Type = "nunclondeltasurface163320_96_wellplate_250ul"
+
+        Competent_Cells_Aliquot_Vol = 40 # uL
+        Media_Aliquot_Vol = 5000 # uL
+
+        # Protocol Parameters #
+
+        DNA_Per_Transformation = 3 # uL
+        Competent_Cells_Volume = 20 # uL
+        Final_Volume = 100 # uL
+        Heat_Shock_Time = 30 # Seconds
+        Heat_Shock_Temp = 42 # celcius
+        Wait_Before_Shock = 300 # seconds
+        Reps = 1
+
+        # Starting Tips #
+
+        Starting_20uL_Tip = "A8"
+        Starting_300uL_Tip = "A1"
+
+        Transformation = otproto_templates.Heat_Shock_Transformation(
+            Protocol=protocol,
+            Name=metadata["protocolName"],
+            Metadata=metadata,
+            DNA_Source_Layouts=DNA_Source_Layouts,
+            Competent_Cells_Source_Type=Competent_Cells_Source_Type,
+            Transformation_Destination_Type=Transformation_Destination_Type,
+            Media_Source_Type=Media_Source_Type,
+            DNA_Volume_Per_Transformation=DNA_Per_Transformation,
+            Competent_Cell_Volume_Per_Transformation=Competent_Cells_Volume,
+            Transformation_Final_Volume=Final_Volume,
+            Heat_Shock_Time=Heat_Shock_Time,
+            Heat_Shock_Temp=Heat_Shock_Temp,
+            Media_Aliquot_Volume=Media_Aliquot_Vol,
+            Competent_Cells_Aliquot_Volume=Competent_Cells_Aliquot_Vol,
+            Wait_Before_Shock=Wait_Before_Shock,
+            Replicates=Reps,
+            Heat_Shock_Modules=["Thermocycler Module"],
+            Starting_20uL_Tip = Starting_20uL_Tip,
+            Starting_300uL_Tip = Starting_300uL_Tip,
+        )
+        Transformation.custom_labware_dir = Custom_Labware_Dir
+        Transformation.run()
+
+        assert len(protocol.commands()) == 903
+
+        assert "Opening Thermocycler lid" in protocol.commands()
+
+        assert "Setting Thermocycler well block temperature to 4.0 °C" in protocol.commands()
+
+    def test_with_multiple_destinations(self):
+        protocol = OT2.get_protocol_api('2.11')
+        protocol.home()
+
+
+        metadata = {
+            'protocolName': 'Transformation Example',
+            'author': 'Bradley Brown',
+            'author-email': 'b.bradley2@newcastle.ac.uk',
+            'user': '',
+            'user-email': '',
+            'source': 'Generated using BiomationScripter',
+            'apiLevel': '2.11',
+            'robotName': 'RobOT2' # This is the name of the OT2 you plan to run the protocol on
+        }
+
+        Custom_Labware_Dir = "Resources/For Docs/custom_labware"
+
+        DNA_Plate_Wells = [
+            "B2",
+            "B3",
+            "B4",
+            "B5",
+            "B6",
+            "B7",
+            "B8",
+            "B9",
+            "B10",
+            "B11",
+            "B12",
+        ]
+
+        DNA_Plate_Content = [
+            "Assembly 1",
+            "Assembly 2",
+            "Assembly 3",
+            "Assembly 4",
+            "Assembly 5",
+            "Assembly 6",
+            "Assembly 7",
+            "Assembly 8",
+            "Assembly 9",
+            "Assembly 10",
+            "Assembly 11",
+        ]
+
+        DNA_Plate = bms.Labware_Layout("DNA Plate", "appliedbiosystemsmicroampoptical_384_wellplate_20ul")
+
+        DNA_Plate.bulk_add_content(
+            Wells = DNA_Plate_Wells,
+            Reagents = DNA_Plate_Content,
+            Volumes = 20
+        )
+
+        # DNA Tubes Info #
+
+        DNA_Tubes_Wells = [
+            "A1",
+            "A2"
+        ]
+
+        DNA_Tubes_Content = [
+            "Positive",
+            "Negative"
+        ]
+
+        DNA_Tubes = bms.Labware_Layout("DNA Tubes", "3dprinted_24_tuberack_1500ul")
+
+        DNA_Tubes.bulk_add_content(
+            Wells = DNA_Tubes_Wells,
+            Reagents = DNA_Tubes_Content,
+            Volumes = 20
+        )
+
+        DNA_Source_Layouts = [DNA_Plate, DNA_Tubes]
+
+        # Other labware and material info #
+
+        Competent_Cells_Source_Type = "3dprinted_24_tuberack_1500ul"
+        Media_Source_Type = "3dprinted_15_tuberack_15000ul"
+        Transformation_Destination_Type = "nunclondeltasurface163320_96_wellplate_250ul"
+
+        Competent_Cells_Aliquot_Vol = 1000 # uL
+        Media_Aliquot_Vol = 5000 # uL
+
+        # Protocol Parameters #
+
+        DNA_Per_Transformation = 3 # uL
+        Competent_Cells_Volume = 20 # uL
+        Final_Volume = 100 # uL
+        Heat_Shock_Time = 30 # Seconds
+        Heat_Shock_Temp = 42 # celcius
+        Wait_Before_Shock = 300 # seconds
+        Reps = 10
+
+        # Starting Tips #
+
+        Starting_20uL_Tip = "A8"
+        Starting_300uL_Tip = "A1"
+
+        Transformation = otproto_templates.Heat_Shock_Transformation(
+            Protocol=protocol,
+            Name=metadata["protocolName"],
+            Metadata=metadata,
+            DNA_Source_Layouts=DNA_Source_Layouts,
+            Competent_Cells_Source_Type=Competent_Cells_Source_Type,
+            Transformation_Destination_Type=Transformation_Destination_Type,
+            Media_Source_Type=Media_Source_Type,
+            DNA_Volume_Per_Transformation=DNA_Per_Transformation,
+            Competent_Cell_Volume_Per_Transformation=Competent_Cells_Volume,
+            Transformation_Final_Volume=Final_Volume,
+            Heat_Shock_Time=Heat_Shock_Time,
+            Heat_Shock_Temp=Heat_Shock_Temp,
+            Media_Aliquot_Volume=Media_Aliquot_Vol,
+            Competent_Cells_Aliquot_Volume=Competent_Cells_Aliquot_Vol,
+            Wait_Before_Shock=Wait_Before_Shock,
+            Replicates=Reps,
+            Heat_Shock_Modules=["temperature module gen2", "temperature module gen2"],
+            Starting_20uL_Tip = Starting_20uL_Tip,
+            Starting_300uL_Tip = Starting_300uL_Tip,
+        )
+        Transformation.custom_labware_dir = Custom_Labware_Dir
+        Transformation.run()
+
+        assert len(protocol.commands()) == 3800
+
+        assert protocol.commands().count("Setting Temperature Module temperature to 4.0 °C (rounded off to nearest integer)") == 4
